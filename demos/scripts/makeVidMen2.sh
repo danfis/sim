34e793825ef5b80b1018ef2efc469e2e75c9c99a
vojta vonasek <vonasek@labe.felk.cvut.cz>
2010-05-27 11:45:47 +0200

    comments to demos/scripts

--- demos/scripts/makeVidMen2.sh
+++ demos/scripts/makeVidMen2.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-#makes video using mencoder using two pass algorithm
+#makes video using mencoder using two pass algorithm from *.png files in current directory
 
-#if video cannot be created - an error like 'aspect ratio error'
-#of duplicated freames error occures,
+#if the video cannot be created due to 'aspect ratio error' or 'duplicated fremes error',
 #then: a) convert all png's to jpg
 #      b) run 'convert pictures.png -depth 8 pictures8.png' on all png's
 
@@ -12,6 +11,9 @@ rm -f frameno.avi
 mencoder "mf://*.png" -mf fps=20 -o test.avi -ovc lavc -lavcopts vcodec=mpeg4:vpass=1:vbitrate=2160000:vqmin=3
 mencoder "mf://*.png" -mf fps=20 -o test.avi -ovc lavc -lavcopts vcodec=mpeg4:vpass=2:vbitrate=2160000:vqmin=3
 
+#this command set proper codec to result .avi file - otherwise it is possible that the video
+#cannot be played on Windows
+#the program 'avifix' is part of transcode (http://www.transcoding.org/)
 
 avifix -i test.avi -F DIVX
 rm divx2pass.log
--- src/comp/povray.hpp
+++ src/comp/povray.hpp
@@ -5,39 +5,37 @@
 #include <vector>
 #include <string>
 
-/**
-  * Povray component
-  *
-  * the task of this component is to draw objects into povray files which can be processed
-  * by 'povray' to obtain high quality picture. The picture can be futhrer joined to video.
-  *
-  * The component has to be registered into simulator, e.g. in constructor:
-  *
-  * sim::PovrayComponent *pc = new sim::PovrayComponent(&bodies);
-  * addComponent(pc);
-  * regPostStep(pc)
-  *
-  *
-  * where 'bodies' is std::vector<sim::VisBody *> - list of pointer to visbody objects
-  * the component has registered 'poststep' callback so the scene will be printed to povray after each simulation
-  * step.
-  *
-  * Exporting bodies geometry+position to povray is done by 'exportToPovray' method in visBody* classes
-  *
-  * The result pictures can be processed by povray:
-  * povray -W1024 -H768 frame_xxxx.pov
-  * or a script demos/script/processPovray.pl can be used (it has several predefined modes to produce low/high quality pictures 
-  *
-  * the result pictures (in png format) can be used to produce a video:
-  * demos/script/makeVidMen2.sh
-  * this script load all png files and run mencoder to make a video in two pass process
-  */
-
 
 namespace sim {
 
 namespace comp {
 
+    
+ /**
+  * Povray component
+  *
+  * the task of this component is to draw objects into povray files which can be processed
+  * by 'povray' to obtain high quality picture. The picture can be further joined to video.
+  *
+  * The component has to be registered into simulator, e.g. in constructor:
+  *
+  * sim::PovrayComponent *pc = new sim::PovrayComponent();
+  * addComponent(pc);
+  *
+  *
+  * the component registers 'poststep' callback (cbPostStep()) so the scene will be printed 
+  * to povray after each simulation step.
+  *
+  * Exporting bodies geometry+position to povray is done by 'exportToPovray' method in visBody* classes
+  *
+  * The result pictures can be processed by povray (http://www.povray.org/) by command:
+  * povray -W1024 -H768 frame_xxxx.pov
+  * or script demos/script/processPovray.pl can be used for processing batch of .pov files.
+  *
+  * the result pictures (in png format) can be used to produce a video using script:
+  * demos/script/makeVidMen2.sh
+  * this script load all png files and run mencoder to make a video in two pass process
+  */
 class Povray : public sim::Component {
 	sim::Sim *_sim;
 	int _frame;
