5459b66cb1c81e87427fdeea945de267860b25e7
Daniel Fiser <danfis@danfis.cz>
2010-06-06 10:30:46 +0200

    Added license notes.

--- src/comp/snake.hpp
+++ src/comp/snake.hpp
@@ -1,3 +1,24 @@
+/***
+ * sim
+ * ---------------------------------
+ * Copyright (c)2010 Vojta Vonasek <vonasek@labe.felk.cvut.cz>
+ *
+ *  This file is part of sim.
+ *
+ *  sim is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation; either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  sim is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _SIM_COMP_SNAKE_HPP_
 #define _SIM_COMP_SNAKE_HPP_
 
