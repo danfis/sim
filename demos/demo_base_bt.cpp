2ee06b299fc321110ca6421770a80c04698c4dcd
Daniel Fiser <danfis@danfis.cz>
2010-06-08 17:15:45 +0200

    demo_base_bt based on demo_base_ode.
    The goal is to merge both demos.

--- demos/demo_base_bt.cpp
+++ demos/demo_base_bt.cpp
@@ -33,27 +33,29 @@ using sim::Vec3;
 using sim::Quat;
 using namespace std;
 
-
 class S : public sim::Sim {
   public:
     S()
-        : sim::Sim()
+        : Sim()
     {
         setTimeStep(sim::Time::fromMs(20));
         setTimeSubSteps(2);
 
         setTimeLimit(sim::Time::fromMs(100000));
+
         pauseSimulation();
 
         World *w = new World();
         sim::Body *b;
 
         setWorld(w);
-        //w->setCFM(1e-10);
-        //w->setCFM(0.01);
-        //w->setERP(0.5);
-        //w->setStepType(World::STEP_TYPE_QUICK);
-        //w->setAutoDisable(0.01, 0.01, 5, 0.);
+        /*
+        w->setCFM(1e-10);
+        w->setCFM(0.01);
+        w->setERP(0.5);
+        w->setStepType(World::STEP_TYPE_QUICK);
+        w->setAutoDisable(0.01, 0.01, 5, 0.);
+        */
         //w->setContactSoftCFM(0.0000001);
         //w->setContactApprox1(false);
         //w->setContactApprox2(false);
@@ -256,7 +258,7 @@ class S : public sim::Sim {
         cy->setPos(pos + sim::Vec3(0., 0., 2.));
 
         j = world()->createJointHinge(cu, cy, pos + sim::Vec3(0., 0., 1.), sim::Vec3(0., 1., 0.));
-        j->setParamLimitLoHi(-M_PI / 2., 0.);
+        ((JointHinge *)j)->setParamLimitLoHi(-M_PI / 2., 0.);
 
         cu->activate();
         cy->activate();
@@ -265,36 +267,156 @@ class S : public sim::Sim {
         cu->visBody()->setText("Hinge lim.", 1., osg::Vec4(0.7, 0.6, 0.3, 1.));
     }
 
+    void createHinge2()
+    {
+        sim::Vec3 pos(6., -6., -3.76);
+        sim::Body *s = world()->createBodyCylinderY(0.5, 0.5, 0.5);
+        sim::Body *b = world()->createBodyBox(Vec3(1.5, 1.5, 3.), 10.);
+        sim::Joint *j;
+
+        b->setPos(pos);
+        s->setPos(pos + sim::Vec3(2.0, 0., 1.));
+
+        j = world()->createJointHinge2(b, s, pos + Vec3(0., 0., 1.),
+                                       Vec3(0., 0., 1.), Vec3(1., 0., 0.));
+
+        b->activate();
+        s->activate();
+        j->activate();
+
+        b->visBody()->setColor(0.5, 0.7, 0.3, 0.3);
+        b->visBody()->setText("Hinge2", 1., osg::Vec4(0.5, 0.7, 0.3, 1.));
+        s->visBody()->setColor(0., 1., 0., 1.);
+
+        sim::Body *b2 = world()->createBodyCube(0.5, 1.);
+        b2->visBody()->setColor(1., 0., 0., 0.3);
+        b2->setPos(pos + Vec3(2.1, -0.3, 2.5));
+        b2->activate();
+    }
+
+    void createHinge2Lim()
+    {
+        sim::Vec3 pos(6., -11., -3.76);
+        sim::Body *s = world()->createBodyCylinderY(0.5, 0.5, 1.5);
+        sim::Body *b = world()->createBodyBox(Vec3(1.5, 1.5, 3.), 10.);
+        sim::Joint *j;
+
+        b->setPos(pos);
+        s->setPos(pos + sim::Vec3(2.0, 0., 1.));
+
+        j = world()->createJointHinge2(b, s, pos + Vec3(0., 0., 1.),
+                                       Vec3(0., 0., 1.), Vec3(1., 0., 0.));
+
+        ((JointHinge2 *)j)->setParamLimitLoHi(-M_PI / 4., M_PI / 4.);
+
+        b->activate();
+        s->activate();
+        j->activate();
+
+        b->visBody()->setColor(0.5, 0.7, 0.3, 0.3);
+        b->visBody()->setText("Hinge2 lim.", 1., osg::Vec4(0.5, 0.7, 0.3, 1.));
+        s->visBody()->setColor(0., 1., 0., 1.);
+
+        sim::Body *b2 = world()->createBodyCube(0.5, 2.);
+        b2->visBody()->setColor(1., 0., 0., 0.3);
+        b2->setPos(pos + Vec3(2.1, -0.3, 2.5));
+        b2->activate();
+    }
+
     void createRobot()
     {
-        /*
-        sim::Vec3 pos(3., 3., -6.);
+        sim::Vec3 pos(3. + 2., 3., -5.);
         sim::Body *chasis;
-        sim::ActuatorWheelCylinderX *w[4];
+        sim::Body *w[4];
+        sim::Joint *jw[4];
         size_t i;
 
         chasis = world()->createBodyBox(sim::Vec3(.6, 1., 0.4), 1.);
         for (i = 0; i < 4; i++){
-            w[i] = world()->createActuatorWheelCylinderX(0.2, 0.2, 1.);
+            w[i] = world()->createBodyCylinderX(0.2, 0.2, 1.);
         }
 
         chasis->setPos(pos);
-        w[0]->setPos(pos.x() + 0.405, pos.y() + 0.4, pos.z() - 0.2);
-        w[1]->setPos(pos.x() - 0.405, pos.y() + 0.4, pos.z() - 0.2);
-        w[2]->setPos(pos.x() + 0.405, pos.y() - 0.4, pos.z() - 0.2);
-        w[3]->setPos(pos.x() - 0.405, pos.y() - 0.4, pos.z() - 0.2);
+        w[0]->setPos(pos.x() + 0.415, pos.y() + 0.4, pos.z() - 0.2);
+        w[1]->setPos(pos.x() - 0.415, pos.y() + 0.4, pos.z() - 0.2);
+        w[2]->setPos(pos.x() + 0.415, pos.y() - 0.4, pos.z() - 0.2);
+        w[3]->setPos(pos.x() - 0.415, pos.y() - 0.4, pos.z() - 0.2);
 
         for (i = 0; i < 4; i++){
-            w[i]->connectToChasis(chasis);
+            jw[i] = world()->createJointHinge(chasis, w[i], w[i]->pos(), Vec3(-1., 0., 0.));
         }
 
         chasis->activate();
         for (i = 0; i < 4; i++){
             w[i]->activate();
+            jw[i]->activate();
         }
 
         chasis->visBody()->setText("Robot", 1., osg::Vec4(0.5, 0.6, 0.3, 1.));
-        */
+    }
+
+    void createRobotMove(const Vec3 &pos)
+    {
+        sim::Body *chasis;
+        sim::Body *w[4];
+        sim::Joint *jw[4];
+        size_t i;
+
+        chasis = world()->createBodyBox(sim::Vec3(.6, 1., 0.4), 1.);
+        for (i = 0; i < 4; i++){
+            w[i] = world()->createBodyCylinderX(0.2, 0.2, 1.);
+        }
+
+        chasis->setPos(pos);
+        w[0]->setPos(pos.x() + 0.415, pos.y() + 0.4, pos.z() - 0.2);
+        w[1]->setPos(pos.x() - 0.415, pos.y() + 0.4, pos.z() - 0.2);
+        w[2]->setPos(pos.x() + 0.415, pos.y() - 0.4, pos.z() - 0.2);
+        w[3]->setPos(pos.x() - 0.415, pos.y() - 0.4, pos.z() - 0.2);
+
+        for (i = 0; i < 4; i++){
+            jw[i] = world()->createJointHinge(chasis, w[i], w[i]->pos(), Vec3(1., 0., 0.));
+        }
+
+        ((JointHinge *)jw[0])->setParamVel(5.);
+        ((JointHinge *)jw[0])->setParamFMax(10.);
+        ((JointHinge *)jw[1])->setParamVel(5.);
+        ((JointHinge *)jw[1])->setParamFMax(10.);
+
+        chasis->activate();
+        for (i = 0; i < 4; i++){
+            w[i]->activate();
+            jw[i]->activate();
+        }
+
+        chasis->visBody()->setText("Robot move", 1., osg::Vec4(0.5, 0.6, 0.3, 1.));
+    }
+
+    void createBunny()
+    {
+        sim::Body *bunny;
+
+        bunny = world()->createBodyTriMesh(bunny_coords, bunny_coords_len,
+                                           bunny_ids, bunny_ids_len, 20.);
+        bunny->visBody()->setColor(0.4, 0.4, 0.4, 1.);
+        bunny->setRot(sim::Quat(Vec3(1., 0., 0.), M_PI * 0.5));
+        bunny->activate();
+    }
+
+    void createSeeSaw()
+    {
+        sim::Vec3 pos(15., -5., -5);
+        sim::Body *b;
+        sim::Joint *j;
+
+        b = world()->createBodyBox(Vec3(2., 5., 0.01), 1.);
+        b->visBody()->setColor(0.7, 0.5, 0.1, 1.);
+        b->visBody()->setTexture("wood.ppm");
+        b->setPos(pos);
+        b->setRot(Quat(Vec3(1., 0., 0.), M_PI / 50.));
+        b->activate();
+
+        j = world()->createJointHinge(b, 0, b->pos(), Vec3(1., 0., 0.));
+        j->activate();
     }
 };
 
@@ -307,4 +429,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
